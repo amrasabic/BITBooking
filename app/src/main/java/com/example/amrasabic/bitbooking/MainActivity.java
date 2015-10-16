01cd6ff953cacfc0d1de1e4dd76e8737ccbe11d9
Amra <amra.sabic@BitCamps-Mac-mini-31.local>
2015-10-16 16:32:04 +0200

    Initial commit

---
+++ app/src/main/java/com/example/amrasabic/bitbooking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.amrasabic.bitbooking;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
